0a6d8af602acc74e132e2ace157e146f79adcd85
Ari van Houten <ari.publicmail@gmail.com>
2022-03-26 09:02:17 +0100

    Added contact icons and copyright infringement

--- src/components/settings/widget_settings/WidgetSettingsComponent.js
+++ src/components/settings/widget_settings/WidgetSettingsComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './widgetsettingscomponent.css';
 import SETTINGS_DESCRIPTOR from '../../../utils/settingsdescriptor';
 import SettingsElement from './SettingsElement';
 
@@ -12,6 +13,26 @@ class WidgetSettingsComponent extends React.Component {
                         <SettingsElement descriptor={ descriptor } descriptorId = { descriptor.id } key={ descriptor.id } />
                     )
                 }
+                <footer id="widget_settings__footer">
+                    <div class="widget_settings__footer_urls">
+                        <a href="https://github.com/aridevelopment-de/myanimetab">
+                            <img src="/icons/github.svg" alt="GitHub" />
+                        </a>
+                        <a href="https://aridevelopment.de/">
+                            <img src="/icons/website.png" alt="Website" />
+                        </a>
+                        <a href="https://twitter.com/AriOnIce">
+                            <img src="/icons/twitter.svg" alt="Twitter" />
+                        </a>
+                        <a href="https://aridevelopment.de/discord">
+                            <img src="/icons/discord.svg" alt="Discord" />
+                        </a>
+                        <a href="mailto:ari.publicmail@gmail.com">
+                            <img src="/icons/email.svg" alt="Mail" />
+                        </a>
+                    </div>
+                    <p id="copyright_infrigement">Copyright © 2022 aridevelopment.de</p>
+                </footer>
             </React.Fragment>
         )
     }
