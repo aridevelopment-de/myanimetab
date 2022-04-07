9be94323aa03b3f14e346aed2b8c311df1b21b2a
Ari van Houten <ari.publicmail@gmail.com>
2022-04-07 19:04:25 +0200

    Added styling for input fields when getting disabled

--- src/components/settings/widget_settings/SettingsItemInput.js
+++ src/components/settings/widget_settings/SettingsItemInput.js
@@ -1,6 +1,6 @@
 import React from "react";
 import getUserSettings from "../../../utils/settings";
-
+import './settingsiteminput.css';
 
 class SettingsItemInput extends React.Component {
     constructor(props) {
@@ -32,14 +32,6 @@ class SettingsItemInput extends React.Component {
                     value={this.state.input} 
                     type={this.props.hidden ? "password" : "text"}
                     spellCheck="false"
-                    style={{
-                        width: "100%",
-                        paddingTop: "10px",
-                        paddingBottom: "10px",
-                        borderRadius: "6px",
-                        boxSizing: "border-box",
-                        border: "1px solid #4a4a4a",
-                    }}
                 />
     }
 }
