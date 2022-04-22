3b8146a404a84c438bb0f84f84a64acb9e945a6e
Ari van Houten <ari.publicmail@gmail.com>
2022-04-22 12:51:26 +0200

    Added am/pm converter to timeutils

--- src/utils/timeutils.js
+++ src/utils/timeutils.js
@@ -1,6 +1,31 @@
 const TimeUtils = {
+    formatAMPM: function(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        
+        return {
+            hours,
+            minutes,
+            ampm
+        };
+    },
     convertTimeToClockFormat: function(date, hour_12) {
         // en-US/de-DE
+        if (hour_12) {
+            let { hours, minutes, ampm } = TimeUtils.formatAMPM(date);
+            let hour24data = this.convertTimeToClockFormat(date, false);
+
+            return {
+                time: `${hours}:${minutes}`,
+                timePeriod: ampm,
+                weekDay: hour24data.weekDay,
+                yearDate: hour24data.yearDate,
+                year: hour24data.year
+            }
+        }
         let timeData = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: hour_12 }).split(" ");
         let dateData = date.toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).split(", ");
         let splitted = timeData[0].split(":");
