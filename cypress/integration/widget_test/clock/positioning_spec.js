32d61867dbb6b25adf34ce24b70ca22c0187c169
Ari van Houten <ari.publicmail@gmail.com>
2022-04-22 17:06:46 +0200

    Added clock positioning tests

--- cypress/integration/widget_test/clock/positioning_spec.js
+++ cypress/integration/widget_test/clock/positioning_spec.js
@@ -16,26 +16,45 @@ describe("Clock settings prepare", () => {
         cy.get(".settings_labels").contains("Settings").click()
     })
 })
-describe("Test the lower left position", () => {
+
+const clock_positions = {
+    left_upper_corner: "one",
+    right_upper_corner: "two",
+    right_lower_corner: "three",
+    left_lower_corner: "four"
+}
+
+describe("Test the left upper corner position", () => {
+    beforeEach(() => {
+        cy
+            .get(".settings_item__title_text")
+            .contains("Clock")
+            .parent()
+            .parent()
+            .children(".settings_item__content")
+            .within(($form) => {
+                cy
+                    .get(".settings_item__form_item_label")
+                    .contains("Positioning")
+                    .parent()
+                    .as("parent")
+            })
+    })
     it("Clicks the dropdown", () => {
         cy
-            .get(".settings_item__form_item_label")
-            .contains("Time Format")
-            .parent()
+            .get("@parent")
             .children(".settings_item__form_item_content")
             .children(".settings_select")
             .children(".settings_select__current_item")
             .click()
     })
-    it("Selects the 24h option", () => {
+    it("Selects the left upper corner option", () => {
         cy
-            .get(".settings_item__form_item_label")
-            .contains("Time Format")
-            .parent()
+            .get("@parent")
             .children(".settings_item__form_item_content")
             .children(".settings_select")
             .children(".settings_select__options")
-            .contains("24h")
+            .contains("Left upper corner")
             .click()
     })
     it("Closes the settings page", () => {
@@ -44,21 +63,156 @@ describe("Test the lower left position", () => {
             .children("svg")
             .click()
     })
-    it("Checks for the right time format", () => {
-        let time = new Date();
-        let hours = time.getHours();
-        let minutes = time.getMinutes();
-
-        // make hours and minutes zero padded
-        hours = hours < 10 ? "0" + hours : hours;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-
-        cy.log("Time: ${hours}:${minutes}")
-
+    it("Checks for the right positioning", () => {
         cy
-            .get(".clock.widget")
-            .children(".time__wrapper")
-            .children("#time_full")
-            .contains(`${hours}:${minutes}`)
+            .get(".clock__wrapper")
+            .should("have.class", clock_positions.left_upper_corner)
+    })
+})
+
+describe("Test the right upper corner position", () => {
+    beforeEach(() => {
+        cy
+            .get(".settings_item__title_text")
+            .contains("Clock")
+            .parent()
+            .parent()
+            .children(".settings_item__content")
+            .within(($form) => {
+                cy
+                    .get(".settings_item__form_item_label")
+                    .contains("Positioning")
+                    .parent()
+                    .as("parent")
+            })
+    })
+    it("Opens the settings page", () => {
+        cy.get(".settings").click()
+    })
+    it("Clicks the dropdown", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__current_item")
+            .click()
+    })
+    it("Selects the right upper corner option", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__options")
+            .contains("Right upper corner")
+            .click()
+    })
+    it("Closes the settings page", () => {
+        cy
+            .get(".settings_menu__close_icon__wrapper")
+            .children("svg")
+            .click()
+    })
+    it("Checks for the right positioning", () => {
+        cy
+            .get(".clock__wrapper")
+            .should("have.class", clock_positions.right_upper_corner)
+    })
+})
+
+describe("Test the right lower corner position", () => {
+    beforeEach(() => {
+        cy
+            .get(".settings_item__title_text")
+            .contains("Clock")
+            .parent()
+            .parent()
+            .children(".settings_item__content")
+            .within(($form) => {
+                cy
+                    .get(".settings_item__form_item_label")
+                    .contains("Positioning")
+                    .parent()
+                    .as("parent")
+            })
+    })
+    it("Opens the settings page", () => {
+        cy.get(".settings").click()
+    })
+    it("Clicks the dropdown", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__current_item")
+            .click()
+    })
+    it("Selects the right lower corner option", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__options")
+            .contains("Right lower corner")
+            .click()
+    })
+    it("Closes the settings page", () => {
+        cy
+            .get(".settings_menu__close_icon__wrapper")
+            .children("svg")
+            .click()
+    })
+    it("Checks for the right positioning", () => {
+        cy
+            .get(".clock__wrapper")
+            .should("have.class", clock_positions.right_lower_corner)
+    })
+})
+
+describe("Test the left lower corner position", () => {
+    beforeEach(() => {
+        cy
+            .get(".settings_item__title_text")
+            .contains("Clock")
+            .parent()
+            .parent()
+            .children(".settings_item__content")
+            .within(($form) => {
+                cy
+                    .get(".settings_item__form_item_label")
+                    .contains("Positioning")
+                    .parent()
+                    .as("parent")
+            })
+    })
+    it("Opens the settings page", () => {
+        cy.get(".settings").click()
+    })
+    it("Clicks the dropdown", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__current_item")
+            .click()
+    })
+    it("Selects the left lower corner option", () => {
+        cy
+            .get("@parent")
+            .children(".settings_item__form_item_content")
+            .children(".settings_select")
+            .children(".settings_select__options")
+            .contains("Left lower corner")
+            .click()
+    })
+    it("Closes the settings page", () => {
+        cy
+            .get(".settings_menu__close_icon__wrapper")
+            .children("svg")
+            .click()
+    })
+    it("Checks for the right positioning", () => {
+        cy
+            .get(".clock__wrapper")
+            .should("have.class", clock_positions.left_lower_corner)
     })
 })
