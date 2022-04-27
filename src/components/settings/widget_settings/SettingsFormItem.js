aff42f0111961be2f57d1dbea51abed3d5824bdf
Ari van Houten <ari.publicmail@gmail.com>
2022-04-27 17:20:21 +0200

    WIP Added word mark

--- src/components/settings/widget_settings/SettingsFormItem.js
+++ src/components/settings/widget_settings/SettingsFormItem.js
@@ -3,16 +3,33 @@ import SettingsItemDropdown from './SettingsItemDropdown';
 import SettingsItemInput from './SettingsItemInput';
 
 
+function SettingsItemLabel(props) {
+    if (props.searchValue == null) {
+        return <p className="settings_item__form_item_label">
+            {props.name}
+        </p>;
+    } else {
+        let highlightedText = props.name.split(" ")
+            .map(word => word.toUpperCase() === props.searchValue.toUpperCase() ? `<span style="background-color: rgba(120, 120, 0, 0.4)">${word}</span>` : word)
+            .join(" ");
+
+        return <p className="settings_item__form_item_label" dangerouslySetInnerHTML={{
+            __html: highlightedText}}
+        />
+    }
+}
+
 class SettingsFormItem extends React.Component {
     // Props
     // formBody: name, id, type, values, displayedValues
     // settingsKey: "cc.name.id"
+    // searchValue: null|string
 
     render() {
         if (this.props.formBody.type === "dropdown") {
             return (
                 <div className="settings_item__form_item">
-                    <p className="settings_item__form_item_label"> {this.props.formBody.name} </p>
+                    <SettingsItemLabel name={this.props.formBody.name} searchValue={this.props.searchValue} />
                     <div className="settings_item__form_item_content">
                         <SettingsItemDropdown values={this.props.formBody.values} displayedValues={this.props.formBody.displayedValues} settingsKey={this.props.settingsKey} />
                     </div>
@@ -21,7 +38,7 @@ class SettingsFormItem extends React.Component {
         } else if (this.props.formBody.type === "input") {
             return (
                 <div className="settings_item__form_item">
-                    <p className="settings_item__form_item_label"> {this.props.formBody.name} </p>
+                    <SettingsItemLabel name={this.props.formBody.name} searchValue={this.props.searchValue} />
                     <div className="settings_item__form_item_content">
                         <SettingsItemInput tooltip={this.props.formBody.tooltip} hidden={this.props.formBody.hidden} settingsKey={this.props.settingsKey} />
                     </div>
