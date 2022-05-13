6afeeb530c10913eaf5d5246139abb0a6ecda37c
Ari24-cb24 <ari.publicmail@gmail.com>
2022-05-13 16:25:37 +0200

    Rewrote export settings to module css

--- src/components/import_export_settings/ExportSettingsComponent.js
+++ src/components/import_export_settings/ExportSettingsComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import EventHandler from "../../utils/eventhandler";
 import ReactJson from 'react-json-view'
 import getUserSettings from "../../utils/settings";
-import './exportsettingscomponent.css';
+import styles from './exportsettingscomponent.module.css';
 
 
 class ExportSettingsComponent extends React.Component {
@@ -19,14 +19,14 @@ class ExportSettingsComponent extends React.Component {
 
     render() {
         return (
-            <div className="export_settings__container">
-                <div className="export_settings__background">
-                    <div className="export_settings">
+            <div className={styles.container}>
+                <div className={styles.background}>
+                    <div className={styles.export_settings}>
                         <header>
-                            <h2 className="export_settings__heading">Export Settings</h2>
+                            <h2 className={styles.heading}>Export Settings</h2>
                         </header>
-                        <div className="export_settings__content">
-                            <p id="export_settings__hint">The json data below is stored in your localstorage</p>
+                        <div className={styles.content}>
+                            <p id={styles.hint}>The json data below is stored in your localstorage</p>
                             <ReactJson 
                                 src={getUserSettings().retrieveWhole()}
                                 style={{border: "1px solid rgba(0,0,0,0.2)", borderRadius: "14px", padding: "10px", marginTop: "10px"}}
@@ -37,9 +37,9 @@ class ExportSettingsComponent extends React.Component {
                                 enableClipboard={false}
                             />
                         </div>
-                        <div className="export_settings__footer">
-                            <button id="export_settings__copy_btn" onClick={this.onCopyClick}>Copy</button>
-                            <button id="export_settings__close_btn" onClick={this.onCloseClick}>Close</button>
+                        <div className={styles.footer}>
+                            <button id={styles.copy_button} onClick={this.onCopyClick}>Copy</button>
+                            <button id={styles.close_button} onClick={this.onCloseClick}>Close</button>
                         </div>
                     </div>
                 </div>
