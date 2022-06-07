822bdb0b200f6099ecfb2ef360031a26c0554c41
Ari24-cb24 <ari.publicmail@gmail.com>
2022-06-07 10:18:48 +0200

    Added triggerEvents function

--- src/utils/eventhandler.js
+++ src/utils/eventhandler.js
@@ -1,6 +1,7 @@
 
 const EventHandler = {
     callbacks: {},
+    callbacksForAll: {},
 
     triggerEvent: function(eventName, data=null) {
         console.debug("[Event] Event published: " + eventName);
@@ -11,6 +12,26 @@ const EventHandler = {
                 this.callbacks[eventName][id](data);
             });
         }
+
+        if (eventName.startsWith("set.")) {
+            // The eventname looks like "set.cc.clock.state"
+            Object.keys(this.callbacksForAll).forEach((id) => {
+                this.callbacksForAll[id](eventName.substring(4), data);
+            })
+        }
+    },
+
+    triggerEvents: function(eventNames, datas=null) {
+        console.debug("[Event] Events published: " + eventNames);
+        console.debug(datas);
+        
+        eventNames.forEach((eventName, index) => {
+            if (datas === null) {
+                this.triggerEvent(eventName);
+            } else {
+                this.triggerEvent(eventName, datas[index]);
+            }
+        });
     },
 
     listenEvent: function(eventName, id, callback) {
@@ -24,6 +45,18 @@ const EventHandler = {
     unlistenEvent: function(eventName, id) {
         if (this.callbacks[eventName]) {
             delete this.callbacks[eventName][id];
+        }
+    },
+
+    listenAll: function(id, callback) {
+        if (!this.callbacksForAll[id]) {
+            this.callbacksForAll[id] = callback;
+        }
+    },
+
+    unlistenAll: function(id) {
+        if (this.callbacksForAll[id]) {
+            delete this.callbacksForAll[id];
         }
     }
 }
