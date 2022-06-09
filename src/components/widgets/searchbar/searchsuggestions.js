b48b0d7f718665cc336b7439bdf2ecd3371c8d4b
Ari24-cb24 <ari.publicmail@gmail.com>
2022-06-09 18:08:51 +0200

    Rewrote Searchbar to Functional Component and custom hooks

--- src/components/widgets/searchbar/searchsuggestions.js
+++ src/components/widgets/searchbar/searchsuggestions.js
@@ -2,25 +2,22 @@ import React from "react";
 import SearchEngine from "../../../utils/searchengine";
 import styles from './searchsuggestion.module.css'
 
-class SearchSuggestions extends React.Component {
-    render() {
-        return (
-            <div className={`${styles.wrapper} ${this.props.showing ? '' : styles.invisible}`}>
-                <div className={styles.suggestions}>
-                    {this.props.suggestions.map(element => 
-                        <div 
-                            className={styles.item__wrapper}
-                            key={element} 
-                            onClick={(e) => SearchEngine.search(e.target.innerHTML)} 
-                            tabIndex="0"
-                        > 
-                            <p>{element}</p>
-                        </div>  
-                    )}
-                </div>
-            </div>
-        )
-    }
+function SearchSuggestions(props) {
+    return (
+       <div className={`${styles.wrapper} ${props.showing ? '' : styles.invisible}`}>
+           <div className={styles.suggestions}>
+               {props.suggestions.map(element => 
+                   <div 
+                       className={styles.item__wrapper}
+                       key={element} 
+                       onClick={(e) => SearchEngine.search(e.target.innerHTML)} 
+                       tabIndex="0" > 
+                       <p>{element}</p>
+                   </div>  
+               )}
+           </div>
+       </div>
+    )
 }
 
 export default SearchSuggestions;
