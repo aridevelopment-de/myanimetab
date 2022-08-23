57380b36b896bbb1bde7a05bc34a3327791e9cf7
Ari24-cb24 <ari.publicmail@gmail.com>
2022-08-23 17:47:32 +0200

    Fixed blur prop passing to childs and disabled blur-effect (#84)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // @ts-nocheck
-import React from 'react';
-import Background from './BackgroundComponent';
-import SettingsComponent from './components/settings/SettingsComponent';
-import EventHandler from './utils/eventhandler';
-import { Component, registry } from './utils/registry/customcomponentregistry';
+import React from "react";
+import Background from "./BackgroundComponent";
+import SettingsComponent from "./components/settings/SettingsComponent";
+import EventHandler from "./utils/eventhandler";
+import { Component, registry } from "./utils/registry/customcomponentregistry";
 
 function App(props) {
-	const [ installedComponents, setInstalledComponents ] = React.useState([]);
+	const [installedComponents, setInstalledComponents] = React.useState([]);
 
 	React.useEffect(() => {
 		registry.setupDefaultComponents().then(() => {
@@ -17,21 +17,32 @@ function App(props) {
 	}, []);
 
 	EventHandler.on("rerenderAll", "app", () => {
-		registry.loadInstalledComponents(() => setInstalledComponents(registry.installedComponents));
-	})
+		registry.loadInstalledComponents(() =>
+			setInstalledComponents(registry.installedComponents)
+		);
+	});
 
 	return (
 		<div className="App">
 			<Background>
-				{installedComponents.map((component: Component) => {					
-					if (component.element === null) {
-						return null;
-					}
+				{(blur) => {
+					return [
+						...installedComponents.map((component: Component) => {
+							if (component.element === null) {
+								return null;
+							}
 
-					return <component.element id={component.fullId} key={component.fullId} />;
-				})}
-
-				<SettingsComponent />
+							return (
+								<component.element
+									id={component.fullId}
+									key={component.fullId}
+									blur={blur}
+								/>
+							);
+						}),
+						<SettingsComponent />,
+					];
+				}}
 			</Background>
 		</div>
 	);
