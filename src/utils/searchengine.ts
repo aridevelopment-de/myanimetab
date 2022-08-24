c26e3e6a394dc968d9b0bcf535603cda00732230
Ari24-cb24 <ari.publicmail@gmail.com>
2022-08-24 09:44:04 +0200

    Rewrote Searchbar to typescript

--- src/utils/searchengine.ts
+++ src/utils/searchengine.ts
@@ -1,33 +1,32 @@
 const ENGINES = [
-    "https://google.com/search?q=",  // Google
-    "https://www.bing.com/search?q=",  // Bing
-    "https://www.ecosia.org/search?q=",  // Ecosia
-    "https://search.yahoo.com/search?p=",  // Yahoo
-    "https://duckduckgo.com/?ia=web&q=",  // DuckDuckGo
-    "https://www.baidu.com/s?wd=",  // Baidu
-    "https://www.ask.com/web?q=",  // Ask
-    "https://www.wolframalpha.com/input/?i=",  // WolframAlpha
+	"https://google.com/search?q=", // Google
+	"https://www.bing.com/search?q=", // Bing
+	"https://www.ecosia.org/search?q=", // Ecosia
+	"https://search.yahoo.com/search?p=", // Yahoo
+	"https://duckduckgo.com/?ia=web&q=", // DuckDuckGo
+	"https://www.baidu.com/s?wd=", // Baidu
+	"https://www.ask.com/web?q=", // Ask
+	"https://www.wolframalpha.com/input/?i=", // WolframAlpha
 ];
 
 const SearchEngine = {
-    search: function(query: string) {
-        query = encodeURIComponent(query);
+	search: function (
+		query: string,
+		search_engine: number = 0,
+		open_with: number = 0
+	) {
+		query = encodeURIComponent(query);
 
-        if (query.length > 0) {
-            const search_engine = 0;
-            const open_with = 1;
+		if (query.length > 0) {
+			let url = `${ENGINES[search_engine]}${query}`;
 
-            let url = `${ENGINES[search_engine]}${query}`;
-            
-            console.error(search_engine, url);
-
-            if (open_with === 1) {
-                window.open(url, "_blank");
-            } else {
-                window.location.href = url;
-            }
-        }
-    }
-}
+			if (open_with === 1) {
+				window.open(url, "_blank");
+			} else {
+				window.location.href = url;
+			}
+		}
+	},
+};
 
 export default SearchEngine;
