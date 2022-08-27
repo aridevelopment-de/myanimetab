9fad002b36d023655c7add0df2a69015b724c7aa
Ari24-cb24 <ari.publicmail@gmail.com>
2022-08-27 16:29:17 +0200

    Made some functions synchronous as they don't need async/await

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,145 +1,152 @@
 import Dexie, { Table } from "dexie";
 import { useEffect, useState } from "react";
-import EventHandler from './eventhandler';
+import EventHandler from "./eventhandler";
 
 interface Widget {
-    id: string; // <type>-<number>
-    settings: any;
+	id: string; // <type>-<number>
+	settings: any;
 }
 
 class WidgetDatabase extends Dexie {
-    widgets!: Table<Widget>;
+	widgets!: Table<Widget>;
 
-    constructor() {
-        super("widgets");
+	constructor() {
+		super("widgets");
 
-        this.version(1).stores({
-            widgets: "id, settings"
-        });
-    }
+		this.version(1).stores({
+			widgets: "id, settings",
+		});
+	}
 
-    async registerWidget(type: string, settings?: { [key: string]: any }) {
-        this.widgets.get(`${type}-0`).then(widget => {
-            if (widget === undefined) {
-                this.addWidget(type, settings);
-            }
-        })
-    }
+	registerWidget(type: string, settings?: { [key: string]: any }) {
+		this.widgets.get(`${type}-0`).then((widget) => {
+			if (widget === undefined) {
+				this.addWidget(type, settings);
+			}
+		});
+	}
 
-    async addWidget(type: string, settings?: { [key: string]: any }) {
-        let amt = 0;
-        this.getIdentifiers().then(identifiers => amt = identifiers.filter(identifier => identifier.id === type).length);
+	addWidget(type: string, settings?: { [key: string]: any }) {
+		let amt = 0;
+		this.getIdentifiers().then(
+			(identifiers) =>
+				(amt = identifiers.filter(
+					(identifier) => identifier.id === type
+				).length)
+		);
 
-        const id = `${type}-${amt}`;
+		const id = `${type}-${amt}`;
 
-        const widget: Widget = {
-            id,
-            settings: settings || {}
-        };
+		const widget: Widget = {
+			id,
+			settings: settings || {},
+		};
 
-        return this.widgets.add(widget);
-    }
+		return this.widgets.add(widget);
+	}
 
-    async getWidget(id: string): Promise<Widget | undefined> {
-        return this.widgets.get(id);
-    }
+	getWidget(id: string): Promise<Widget | undefined> {
+		return this.widgets.get(id);
+	}
 
-    async getIdentifiers(): Promise<Array<{ id: string, number: string }>> {
-        return this.widgets.toArray().then(widgets => {
-            return widgets.map(widget => {
-                return {
-                    id: widget.id.split("-")[0],
-                    number: widget.id.split("-")[1]
-                };
-            });
-        });
-    }
+	async getIdentifiers(): Promise<Array<{ id: string; number: string }>> {
+		return this.widgets.toArray().then((widgets) => {
+			return widgets.map((widget) => {
+				return {
+					id: widget.id.split("-")[0],
+					number: widget.id.split("-")[1],
+				};
+			});
+		});
+	}
 
-    async getSetting(id: string, key: string): Promise<any | undefined> {
-        return this.widgets.get(id).then((widget: Widget | undefined) => {
-            if (widget === undefined) {
-                return undefined;
-            }
-            
-            return widget.settings[key];
-        });
-    }
+	async getSetting(id: string, key: string): Promise<any | undefined> {
+		return this.widgets.get(id).then((widget: Widget | undefined) => {
+			if (widget === undefined) {
+				return undefined;
+			}
 
-    async setSetting(id: string, key: string, value: any) {
-        EventHandler.emit(`widget.${id}.${key}`, value);
-        
-        key = `settings.${key}`;
-        this.widgets.update(id, { [key]: value });
-    }
+			return widget.settings[key];
+		});
+	}
+
+	setSetting(id: string, key: string, value: any) {
+		EventHandler.emit(`widget.${id}.${key}`, value);
+
+		key = `settings.${key}`;
+		this.widgets.update(id, { [key]: value });
+	}
 }
 
 interface Meta {
-    name: string;
-    value: any;
+	name: string;
+	value: any;
 }
 
 class MetaDatabase extends Dexie {
-    meta!: Table<Meta>;
-    changes: { [key: string]: Array<Function> } = {};
+	meta!: Table<Meta>;
+	changes: { [key: string]: Array<Function> } = {};
 
-    constructor() {
-        super("meta");
+	constructor() {
+		super("meta");
 
-        this.version(1).stores({
-            meta: "name, value"
-        });
-    }
+		this.version(1).stores({
+			meta: "name, value",
+		});
+	}
 
-    onMetaChange(name: string, callback: (value: any) => void) {
-        if (this.changes[name] === undefined) {
-            this.changes[name] = [];
-        }
+	onMetaChange(name: string, callback: (value: any) => void) {
+		if (this.changes[name] === undefined) {
+			this.changes[name] = [];
+		}
 
-        this.changes[name].push(callback);
-    }
+		this.changes[name].push(callback);
+	}
 
-    emitOnMetaChange(name: string, newValue: any) {
-        if (this.changes[name] !== undefined) {
-            this.changes[name].forEach(callback => callback(newValue));
-        }
-    }
+	emitOnMetaChange(name: string, newValue: any) {
+		if (this.changes[name] !== undefined) {
+			this.changes[name].forEach((callback) => callback(newValue));
+		}
+	}
 
-    async getMeta(name: string): Promise<any | undefined> {
-        return this.meta.get(name).then(meta => (meta || { value: undefined }).value);
-    }
+	async getMeta(name: string): Promise<any | undefined> {
+		return this.meta
+			.get(name)
+			.then((meta) => (meta || { value: undefined }).value);
+	}
 
-    async setMeta(name: string, value: any) {
-        this.meta.update(name, { value: value });
-        this.emitOnMetaChange(name, value);
-    }
+	setMeta(name: string, value: any) {
+		this.meta.update(name, { value: value });
+		this.emitOnMetaChange(name, value);
+	}
 
-    async registerMeta(name: string, value: any) {
-        this.getMeta(name).then(meta => {
-            if (meta === undefined) {
-                this.meta.put({ name: name, value: value });
-            }
-        });
-    }
+	registerMeta(name: string, value: any) {
+		this.getMeta(name).then((meta) => {
+			if (meta === undefined) {
+				this.meta.put({ name: name, value: value });
+			}
+		});
+	}
 }
 
 export const useMeta = (key: string, mapFunction?: Function): any => {
-    const [status, setStatus] = useState();
-    useEffect(() => { 
-        metaDb.getMeta(key).then(result => {
-            const newData = (mapFunction || ((d: any) => d))(result) || result;
-            setStatus(newData)
-        }); 
-    }, [key, mapFunction]);
+	const [status, setStatus] = useState();
+	useEffect(() => {
+		metaDb.getMeta(key).then((result) => {
+			const newData = (mapFunction || ((d: any) => d))(result) || result;
+			setStatus(newData);
+		});
+	}, [key, mapFunction]);
 
-    useEffect(() => {
-        metaDb.onMetaChange(key, (data: any) => {
-            const newData = (mapFunction || ((d: any) => d))(data) || data;
-            setStatus(newData);
-        });
-    }, [key, mapFunction]);
+	useEffect(() => {
+		metaDb.onMetaChange(key, (data: any) => {
+			const newData = (mapFunction || ((d: any) => d))(data) || data;
+			setStatus(newData);
+		});
+	}, [key, mapFunction]);
 
-    return status;
-}
+	return status;
+};
 
 export const widgetsDb = new WidgetDatabase();
 export const metaDb = new MetaDatabase();
