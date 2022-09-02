9661d96ad899a64874ad569800772a31877cb08d
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-02 11:25:43 +0200

    Fixed useSetting state not being updated immediatly as it depended on the widgetsDB to update

--- src/utils/eventhooks.ts
+++ src/utils/eventhooks.ts
@@ -4,9 +4,12 @@ import { useEffect, useState } from "react";
 import { widgetsDb } from "./db";
 import EventHandler from "./eventhandler";
 
-
-export const useSetting = (id: string, key: string, trigger_function?: Function): [any, Function] => {
-    /* const [state, setState] = useState();
+export const useSetting = (
+	id: string,
+	key: string,
+	trigger_function?: Function
+): [any, Function] => {
+	/* const [state, setState] = useState();
     const changeStatus = (value: any) => widgetsDb.setSetting(id, key, value);
 
     useEvent(`widget.${id}.${key}`, HashCode.value(id + key + String(trigger_function)), null, (data: any) => {
@@ -22,41 +25,49 @@ export const useSetting = (id: string, key: string, trigger_function?: Function)
     }, [id, key, trigger_function])
     
     return [state, changeStatus]; */
-    
-    const [state, setState] = useState();
-    const data = useLiveQuery(() => widgetsDb.widgets.where("id").equals(id).toArray())
-    
-    const changeData = (newValue: any) => {
-        widgetsDb.setSetting(id, key, newValue);
-    }
 
-    useEffect(() => {
-        if (data !== undefined) {
-            setState(data[0].settings[key]);
-        }
-    }, [data, key])
+	const [state, setState] = useState();
+	const data = useLiveQuery(() =>
+		widgetsDb.widgets.where("id").equals(id).toArray()
+	);
 
-    return [state, changeData];
-}
+	const changeData = (newValue: any) => {
+		setState(newValue);
+		widgetsDb.setSetting(id, key, newValue);
+	};
 
-export const useEvent = (event_name: string, identifier: string, default_value?: any, trigger_function?: Function): [any, Function] => {
-    const [state, setState] = useState(default_value);
-    const changeEvent = (value: any) => EventHandler.emit(event_name, {value: value, sender: identifier});
+	useEffect(() => {
+		if (data !== undefined) {
+			setState(data[0].settings[key]);
+		}
+	}, [data, key]);
 
-    useEffect(() => {
-        EventHandler.on(event_name, identifier, (data: any) => {
-            const value = (trigger_function || (() => {}))(data) || data.value;
-            setState(value);
-        });
+	return [state, changeData];
+};
 
-        return () => {
-            EventHandler.off(event_name, identifier);
-        }
-    }, [event_name, identifier, trigger_function]);
+export const useEvent = (
+	event_name: string,
+	identifier: string,
+	default_value?: any,
+	trigger_function?: Function
+): [any, Function] => {
+	const [state, setState] = useState(default_value);
+	const changeEvent = (value: any) =>
+		EventHandler.emit(event_name, { value: value, sender: identifier });
 
-    return [state, changeEvent];
-}
+	useEffect(() => {
+		EventHandler.on(event_name, identifier, (data: any) => {
+			const value = (trigger_function || (() => {}))(data) || data.value;
+			setState(value);
+		});
 
+		return () => {
+			EventHandler.off(event_name, identifier);
+		};
+	}, [event_name, identifier, trigger_function]);
+
+	return [state, changeEvent];
+};
 
 /**
  * Javascript HashCode v1.0.0
@@ -69,33 +80,39 @@ export const useEvent = (event_name: string, identifier: string, default_value?:
  *
  * pmav, 2010
  */
- var HashCode = function() {
-    var serialize = function(object: any) {
-        // Private
-        var type, serializedCode = "";
+var HashCode = (function () {
+	var serialize = function (object: any) {
+		// Private
+		var type,
+			serializedCode = "";
 
-        type = typeof object;
+		type = typeof object;
 
-        if (type === 'object') {
-            var element;
+		if (type === "object") {
+			var element;
 
-            for (element in object) {
-                serializedCode += "[" + type + ":" + element + serialize(object[element]) + "]";
-            }
+			for (element in object) {
+				serializedCode +=
+					"[" +
+					type +
+					":" +
+					element +
+					serialize(object[element]) +
+					"]";
+			}
+		} else if (type === "function") {
+			serializedCode += "[" + type + ":" + object.toString() + "]";
+		} else {
+			serializedCode += "[" + type + ":" + object + "]";
+		}
 
-        } else if (type === 'function') {
-            serializedCode += "[" + type + ":" + object.toString() + "]";
-        } else {
-            serializedCode += "[" + type + ":" + object+"]";
-        }
+		return serializedCode.replace(/\s/g, "");
+	};
 
-        return serializedCode.replace(/\s/g, "");
-    };
-
-    // Public, API
-    return {
-        value : function(object: any) {
-            return md5(serialize(object));
-        }
-    };
-}();
+	// Public, API
+	return {
+		value: function (object: any) {
+			return md5(serialize(object));
+		},
+	};
+})();
