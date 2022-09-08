12c06b673ab0bb7c9d38dce26df9dc52fe8b32e5
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-08 18:28:12 +0200

    Removed auto-switiching background from BackgroundComponent

--- src/BackgroundComponent.tsx
+++ src/BackgroundComponent.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useState } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
+import { useEffect, useState, useCallback } from "react";
 import styles from "./App.module.css";
 import AutoHideScheduler from "./components/AutoHideScheduler";
-import { IImage, metaDb, useMeta, widgetsDb } from "./utils/db";
+import { IImage, IQueue, metaDb, useMeta, widgetsDb } from "./utils/db";
 import EventHandler from "./utils/eventhandler";
 import { useSetting } from "./utils/eventhooks";
 
@@ -12,68 +13,19 @@ const playlistOrderValues = ["Ordered", "Shuffled"];
 
 function Background(props: any) {
 	const [blur, setBlur] = useState(false);
-	const [currentBackgroundUrl, setCurrentBackgroundUrl] = useState("");
-	const currentBackgroundIdx = useMeta("selected_image", (id: number) => {
-		metaDb
-			.getImage(id)
-			.then((image: IImage | undefined) =>
-				setCurrentBackgroundUrl(image ? image.url : "")
-			);
-	});
+	const background = useLiveQuery(async () =>
+		metaDb.images.get((await metaDb.meta.get("selected_image"))?.value)
+	);
+	/* 
 	const [whenWallpaperSwitch, _1] = useSetting("wallpaper-0", "when_switch");
 	const [shouldWallpaperSwitch, _2] = useSetting("wallpaper-0", "state");
-
-	const nextImage = () => {
-		widgetsDb
-			.getSetting("wallpaper-0", "playlist_order")
-			.then((value: number) => {
-				const playlistOrder: string = playlistOrderValues[value];
-
-				/* metaDb.getMeta("images").then((images: string[]) => {
-					if (images.length >= 1) {
-						if (playlistOrder === "Ordered") {
-							let idx = currentBackgroundIdx || 0;
-							idx = (idx + 1) % images.length;
-							metaDb.setMeta("selected_image", idx);
-						} else if (playlistOrder === "Shuffled") {
-							const idx = Math.floor(
-								Math.random() * images.length
-							);
-							metaDb.setMeta("selected_image", idx);
-						}
-					}
-				}); */
-			});
-	};
-
-	EventHandler.on("skip_image", "background", nextImage);
-	EventHandler.on("select_image", "background", (data: { idx: number }) => {
-		metaDb.setMeta("selected_image", data.idx);
-		console.log(data.idx);
-	});
-
-	// Background switch interval
-	useEffect(() => {
-		let interval: ReturnType<typeof setInterval>;
-		const switchInterval: number | null = switchValues[whenWallpaperSwitch];
-
-		if (shouldWallpaperSwitch) {
-			if (switchInterval !== null) {
-				interval = setInterval(
-					nextImage,
-					1000 * switchInterval || 60000
-				);
-			}
-
-			return () => clearInterval(interval);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [whenWallpaperSwitch, shouldWallpaperSwitch]);
-
+ */
 	return (
 		<div
 			className={styles.background}
-			style={{ backgroundImage: `url(${currentBackgroundUrl})` }}
+			style={{
+				backgroundImage: `url(${(background || { url: "" }).url})`,
+			}}
 		>
 			<AutoHideScheduler setBlur={setBlur} blur={blur} />
 			{props.children(blur)}
