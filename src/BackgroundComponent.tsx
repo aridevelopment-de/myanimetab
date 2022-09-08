43b95bd3de9d5a3d030b1e713c7259a1fbc14e40
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-08 18:57:21 +0200

    Added automatic background-switching

--- src/BackgroundComponent.tsx
+++ src/BackgroundComponent.tsx
@@ -3,23 +3,17 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { useEffect, useState, useCallback } from "react";
 import styles from "./App.module.css";
 import AutoHideScheduler from "./components/AutoHideScheduler";
+import BackgroundScheduler from "./components/BackgroundScheduler";
 import { IImage, IQueue, metaDb, useMeta, widgetsDb } from "./utils/db";
 import EventHandler from "./utils/eventhandler";
 import { useSetting } from "./utils/eventhooks";
-
-const blurValues = [5, 10, 30, 60, 300];
-const switchValues = [null, 10, 60, 120, 300, 600, 1800, 3600];
-const playlistOrderValues = ["Ordered", "Shuffled"];
 
 function Background(props: any) {
 	const [blur, setBlur] = useState(false);
 	const background = useLiveQuery(async () =>
 		metaDb.images.get((await metaDb.meta.get("selected_image"))?.value)
 	);
-	/* 
-	const [whenWallpaperSwitch, _1] = useSetting("wallpaper-0", "when_switch");
-	const [shouldWallpaperSwitch, _2] = useSetting("wallpaper-0", "state");
- */
+
 	return (
 		<div
 			className={styles.background}
@@ -28,6 +22,7 @@ function Background(props: any) {
 			}}
 		>
 			<AutoHideScheduler setBlur={setBlur} blur={blur} />
+			<BackgroundScheduler />
 			{props.children(blur)}
 		</div>
 	);
