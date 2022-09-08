a0e10905f997fe97933c274da147a087d2e06189
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-08 16:38:35 +0200

    Update playlist image whenever image gets removed via queue

--- src/components/settings/playlist_settings/filetypes/background.tsx
+++ src/components/settings/playlist_settings/filetypes/background.tsx
@@ -1,9 +1,10 @@
-import styles from "./background.module.css";
-import globalstyles from "../playlistsettingscomponent.module.css";
-import { IImage, metaDb } from "../../../../utils/db";
 import { Checkbox } from "@mantine/core";
-import { useEffect, useState } from "react";
 import { showNotification } from "@mantine/notifications";
+import { useEffect, useRef, useState } from "react";
+import { IImage, metaDb } from "../../../../utils/db";
+import EventHandler from "../../../../utils/eventhandler";
+import globalstyles from "../playlistsettingscomponent.module.css";
+import styles from "./background.module.css";
 
 const Background = (props: {
 	image: IImage;
@@ -13,6 +14,7 @@ const Background = (props: {
 }) => {
 	const [isInQueue, setIsInQueue] = useState<boolean>(false);
 	const [hovered, setHovered] = useState<boolean>(false);
+	const checkboxHover = useRef<boolean>();
 
 	useEffect(() => {
 		metaDb.getMeta("selected_queue").then((value: any) => {
@@ -22,6 +24,23 @@ const Background = (props: {
 					.then(setIsInQueue);
 			}
 		});
+
+		EventHandler.on(
+			"queue.removeImage",
+			`image-filetype-#${props.image.id}`,
+			(data: { value: number }) => {
+				if (data.value === props.image.id) {
+					setIsInQueue(false);
+				}
+			}
+		);
+
+		return () => {
+			EventHandler.off(
+				"queue.removeImage",
+				`image-filetype-#${props.image.id}`
+			);
+		};
 	}, [props.image]);
 
 	const setQueueStatus = (addToQueue: boolean) => {
@@ -59,6 +78,7 @@ const Background = (props: {
 		<div
 			className={`${globalstyles.element_container} ${styles.image_container}`}
 			data-context-filetype="image"
+			data-id={props.image.id}
 			style={{
 				border: props.selected
 					? "3px solid var(--mantine-color-blue-5)"
@@ -85,6 +105,11 @@ const Background = (props: {
 				onDragStart={() => props.setDraggedElement(props.image)}
 				onMouseEnter={() => setHovered(true)}
 				onMouseLeave={() => setHovered(false)}
+				onClick={() => {
+					if (!checkboxHover.current) {
+						metaDb.setMeta("selected_image", props.image.id);
+					}
+				}}
 			>
 				<div
 					style={{
@@ -92,14 +117,19 @@ const Background = (props: {
 						width: "100%",
 						height: "100%",
 					}}
+					data-context-filetype="image"
+					data-id={props.image.id}
 				>
 					{(isInQueue || hovered) && (
 						<Checkbox
+							onMouseEnter={() => (checkboxHover.current = true)}
+							onMouseLeave={() => (checkboxHover.current = false)}
 							size="xs"
 							style={{
 								position: "absolute",
 								top: "7px",
 								right: "7px",
+								zIndex: 100,
 							}}
 							checked={isInQueue}
 							onChange={(event) =>
