03eb1ae67b88395dba942a45adcc74ad7d2cd86e
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-08 15:25:21 +0200

    Added overlay button to images to add to current queue

--- src/components/settings/playlist_settings/filetypes/background.tsx
+++ src/components/settings/playlist_settings/filetypes/background.tsx
@@ -1,6 +1,9 @@
 import styles from "./background.module.css";
 import globalstyles from "../playlistsettingscomponent.module.css";
-import { IImage } from "../../../../utils/db";
+import { IImage, metaDb } from "../../../../utils/db";
+import { Checkbox } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { showNotification } from "@mantine/notifications";
 
 const Background = (props: {
 	image: IImage;
@@ -8,6 +11,50 @@ const Background = (props: {
 	selected: boolean;
 	setDraggedElement: Function;
 }) => {
+	const [isInQueue, setIsInQueue] = useState<boolean>(false);
+	const [hovered, setHovered] = useState<boolean>(false);
+
+	useEffect(() => {
+		metaDb.getMeta("selected_queue").then((value: any) => {
+			if (value !== null) {
+				metaDb
+					.queueContainsImage(value, props.image)
+					.then(setIsInQueue);
+			}
+		});
+	}, [props.image]);
+
+	const setQueueStatus = (addToQueue: boolean) => {
+		metaDb.getMeta("selected_queue").then((value: any) => {
+			if (value === null) {
+				showNotification({
+					title: "No queue selected",
+					message:
+						"Please select a queue first to add the image to it",
+					color: "red",
+				});
+			} else {
+				if (addToQueue) {
+					metaDb
+						.insertImageToQueue(value, props.image)
+						.then((success: boolean) => {
+							if (success) {
+								setIsInQueue(true);
+							}
+						});
+				} else {
+					metaDb
+						.removeImageFromQueue(value, props.image)
+						.then((success: boolean) => {
+							if (success) {
+								setIsInQueue(false);
+							}
+						});
+				}
+			}
+		});
+	};
+
 	return (
 		<div
 			className={`${globalstyles.element_container} ${styles.image_container}`}
@@ -36,7 +83,32 @@ const Background = (props: {
 				data-id={props.image.id}
 				draggable
 				onDragStart={() => props.setDraggedElement(props.image)}
-			/>
+				onMouseEnter={() => setHovered(true)}
+				onMouseLeave={() => setHovered(false)}
+			>
+				<div
+					style={{
+						position: "relative",
+						width: "100%",
+						height: "100%",
+					}}
+				>
+					{(isInQueue || hovered) && (
+						<Checkbox
+							size="xs"
+							style={{
+								position: "absolute",
+								top: "7px",
+								right: "7px",
+							}}
+							checked={isInQueue}
+							onChange={(event) =>
+								setQueueStatus(event.target.checked)
+							}
+						/>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
