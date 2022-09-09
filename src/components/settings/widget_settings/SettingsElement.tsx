9d1ee199a0d011a625416c6f7cc842ceaef06191
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-09 15:22:27 +0200

    Made widgets collapseable and set a new grayish contrast

--- src/components/settings/widget_settings/SettingsElement.tsx
+++ src/components/settings/widget_settings/SettingsElement.tsx
@@ -1,38 +1,64 @@
-import { Menu, Switch } from "@mantine/core";
+import { Menu, Stack, Switch } from "@mantine/core";
 import { useSetting } from "../../../utils/eventhooks";
 import { Component } from "../../../utils/registry/types";
 import styles from "./settingselement.module.css";
-import SettingsFormItem from "./SettingsFormItem";
+import SettingsFormItem, { SettingsItemLabel } from "./SettingsFormItem";
 import EventHandler from "../../../utils/eventhandler";
 import SettingsIcon from "@mui/icons-material/Settings";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { registry } from "../../../utils/registry/customcomponentregistry";
+import { useEffect, useRef, useState } from "react";
+
+const MAX_HEIGHT = 1200; // about 6 dropdowns
 
 function SettingsElement(props: { data: Component; searchValue: string }) {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [checked, setChecked] = useSetting(props.data.fullId, "state");
+	const [minimized, setMinimized] = useState<boolean>(false);
+	const contentRef = useRef<HTMLDivElement>();
+	const toolbarHovered = useRef<boolean>(false);
+	const initialHeight = useRef<number>();
+
+	useEffect(() => {
+		if (
+			!minimized &&
+			contentRef.current &&
+			initialHeight.current === null
+		) {
+			initialHeight.current = Math.min(
+				MAX_HEIGHT,
+				contentRef.current.clientHeight
+			);
+		}
+	}, [contentRef, contentRef.current, minimized]);
 
 	return (
 		<div className={`${styles.item} ${!checked ? "disabled" : ""}`}>
-			<div className={styles.title}>
-				<p className={styles.title_text}>
-					{props.data.headerSettings.name}
-				</p>
+			{/* Header */}
+			<div
+				className={styles.title}
+				onClick={() =>
+					toolbarHovered.current === false
+						? setMinimized(!minimized)
+						: void 0
+				}
+			>
+				<SettingsItemLabel
+					className={styles.title_text}
+					name={props.data.headerSettings.name}
+					searchValue={props.searchValue || undefined}
+				/>
+				{/* Toolbar */}
 				<div
 					style={{
 						display: "flex",
 						gap: "7px",
 					}}
+					/* @ts-ignore */
+					onMouseEnter={() => (toolbarHovered.current = true)}
+					onMouseLeave={() => (toolbarHovered.current = false)}
 				>
-					{checked !== undefined ? (
-						<Switch
-							checked={checked}
-							onChange={(e) => {
-								setChecked(e.currentTarget.checked);
-								EventHandler.emit("rerenderAll");
-							}}
-						/>
-					) : null}
+					{/* As deleting is currently the only option, the settings button will only available when the component is deletable */}
 					{props.data.metadata.removeableComponent ? (
 						<Menu shadow="md" width={200} position="left-start">
 							<Menu.Target>
@@ -73,9 +99,38 @@ function SettingsElement(props: { data: Component; searchValue: string }) {
 							</Menu.Dropdown>
 						</Menu>
 					) : null}
+					{checked !== undefined ? (
+						<Switch
+							checked={checked}
+							onChange={(e) => {
+								setChecked(e.currentTarget.checked);
+								EventHandler.emit("rerenderAll");
+							}}
+						/>
+					) : null}
 				</div>
 			</div>
-			<div>
+
+			{/* Body */}
+			<Stack
+				spacing="xs"
+				className={`${styles.content} ${
+					minimized ? styles.content__minimized : undefined
+				}`}
+				style={{
+					maxHeight: minimized
+						? undefined
+						: `${initialHeight.current || MAX_HEIGHT}px`,
+					overflowY: minimized
+						? "hidden"
+						: contentRef.current &&
+						  contentRef.current?.clientHeight >= MAX_HEIGHT
+						? "auto"
+						: undefined,
+				}}
+				/* @ts-ignore */
+				ref={contentRef}
+			>
 				{props.data.contentSettings?.map(
 					(componentSetting, index: number) => {
 						if (props.searchValue == null) {
@@ -103,12 +158,25 @@ function SettingsElement(props: { data: Component; searchValue: string }) {
 									key={index}
 								/>
 							);
+						} else if (
+							props.data.headerSettings.name
+								.toLowerCase()
+								.includes(props.searchValue.toLowerCase())
+						) {
+							return (
+								<SettingsFormItem
+									componentSetting={componentSetting}
+									componentId={props.data.fullId}
+									disabled={checked === false}
+									key={index}
+								/>
+							);
 						}
 
 						return null;
 					}
 				)}
-			</div>
+			</Stack>
 		</div>
 	);
 }
