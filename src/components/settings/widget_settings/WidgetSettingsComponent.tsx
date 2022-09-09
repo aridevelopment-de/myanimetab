51301cd760905473fcd06fcc28bf84a4b0ef2a0b
Ari24-cb24 <ari.publicmail@gmail.com>
2022-09-09 14:56:16 +0200

    Removed import/export modals and buttons

--- src/components/settings/widget_settings/WidgetSettingsComponent.tsx
+++ src/components/settings/widget_settings/WidgetSettingsComponent.tsx
@@ -1,18 +1,6 @@
 // @ts-nocheck
-import {
-	Button,
-	Group,
-	Menu,
-	Modal,
-	Stack,
-	Text,
-	Textarea,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { showNotification } from "@mantine/notifications";
-import React, { useEffect, useState } from "react";
-import ReactJson from "react-json-view";
-import { widgetsDb } from "../../../utils/db";
+import { Button, Menu, Stack } from "@mantine/core";
+import React, { useState } from "react";
 import EventHandler from "../../../utils/eventhandler";
 import { registry } from "../../../utils/registry/customcomponentregistry";
 import { Component, KnownComponent } from "../../../utils/registry/types";
@@ -43,164 +31,9 @@ const containsString = (string: string, component: Component) => {
 
 const WidgetSettingsComponent = (props: { bodyRef: any }) => {
 	const [searchbarValue, setSearchbarValue] = useState("");
-	const [exportModalState, setExportModalState] = useState(false);
-	const [importModalState, setImportModalState] = useState(false);
-	const [jsonData, setJsonData] = useState({});
-
-	useEffect(() => {
-		if (exportModalState === true) {
-			(async () => {
-				setJsonData(await widgetsDb.toJson());
-			})();
-		}
-	}, [exportModalState]);
-
-	const importSettingsForm = useForm({
-		initialValues: {
-			json: "",
-		},
-		validate: {
-			json: (value) => {
-				try {
-					JSON.parse(value);
-				} catch (e) {
-					return "Invalid JSON";
-				}
-				return null;
-			},
-		},
-	});
 
 	return (
 		<React.Fragment>
-			{/* Export Modal */}
-			<Modal
-				opened={exportModalState}
-				onClose={() => setExportModalState(false)}
-				title="Export Settings"
-			>
-				<Stack>
-					<Text color="dimmed">
-						The json data below is being stored in your IndexedDB.
-						MyAnimeTab does not collect any data and sends it to any
-						server, thus everything is saved on the client.
-					</Text>
-					<ReactJson
-						src={jsonData}
-						style={{
-							border: "1px solid rgba(0,0,0,0.2)",
-							borderRadius: "14px",
-							padding: "10px",
-							marginTop: "10px",
-						}}
-						name={false}
-						collapsed={true}
-						collapseStringsAfterLength={65}
-						displayDataTypes={false}
-						enableClipboard={false}
-					/>
-					<Group position="right">
-						<Button
-							color="green"
-							onClick={() => {
-								navigator.clipboard.writeText(
-									JSON.stringify(jsonData)
-								);
-
-								showNotification({
-									title: "Data copied to clipboard!",
-								});
-							}}
-						>
-							Copy
-						</Button>
-						<Button onClick={() => setExportModalState(false)}>
-							Close
-						</Button>
-					</Group>
-				</Stack>
-			</Modal>
-
-			{/* Import Modal */}
-			<Modal
-				opened={importModalState}
-				onClose={() => setImportModalState(false)}
-				title="Import Settings"
-			>
-				<form
-					onSubmit={importSettingsForm.onSubmit(
-						(
-							values: { json: string },
-							event: React.FormEvent<HTMLFormElement>
-						) => {
-							showNotification({
-								color: "yellow",
-								title: "Trying to load json data...",
-							});
-
-							setTimeout(() => {
-								try {
-									widgetsDb.fromJSON(JSON.parse(values.json));
-								} catch (e) {
-									console.error(e);
-
-									showNotification({
-										color: "red",
-										title: "An error occured while loading data!",
-										message:
-											"For more details, take a look at the console. If you need any help, reach out to us at the support discord server!",
-									});
-
-									return;
-								}
-
-								showNotification({
-									color: "green",
-									title: "Successfully loaded json data!",
-									message:
-										"Page will be reloaded in a moment to update the existing data.",
-								});
-
-								setTimeout(
-									() => window.location.reload(),
-									3000
-								);
-							}, 1000);
-						}
-					)}
-				>
-					<Stack>
-						<Text color="dimmed">
-							It may not always be possible that you are able to
-							import data as the structure differs from version to
-							version. If you need any help, feel free to ask on
-							the{" "}
-							<a href="https://aridevelopment.de/dc">
-								Support Discord Server
-							</a>
-						</Text>
-						<Textarea
-							placeholder="Enter valid json"
-							label="Enter the settings data below"
-							withAsterisk
-							{...importSettingsForm.getInputProps("json", {
-								type: "input",
-							})}
-						/>
-						<Group position="right">
-							<Button
-								variant="outline"
-								color="gray"
-								onClick={() => setImportModalState(false)}
-							>
-								Close
-							</Button>
-							<Button type="submit">Submit</Button>
-						</Group>
-					</Stack>
-				</form>
-			</Modal>
-
 			{/* Widget Settings */}
 			<div className={styles.toolbar}>
 				<input
@@ -247,37 +80,31 @@ const WidgetSettingsComponent = (props: { bodyRef: any }) => {
 					</Menu.Dropdown>
 				</Menu>
 			</div>
-			{registry.installedComponents.map((component: Component) => {
-				if (searchbarValue.trim() === "") {
-					return (
-						<SettingsElement
-							data={component}
-							key={component.fullId}
-							searchValue={null}
-						/>
-					);
-				}
+			<Stack>
+				{registry.installedComponents.map((component: Component) => {
+					if (searchbarValue.trim() === "") {
+						return (
+							<SettingsElement
+								data={component}
+								key={component.fullId}
+								searchValue={null}
+							/>
+						);
+					}
 
-				if (containsString(searchbarValue, component)) {
-					return (
-						<SettingsElement
-							data={component}
-							key={component.fullId}
-							searchValue={searchbarValue}
-						/>
-					);
-				}
+					if (containsString(searchbarValue, component)) {
+						return (
+							<SettingsElement
+								data={component}
+								key={component.fullId}
+								searchValue={searchbarValue}
+							/>
+						);
+					}
 
-				return null;
-			})}
-			<div className={styles.control_group}>
-				<Button onClick={() => setImportModalState(true)} color="green">
-					Import
-				</Button>
-				<Button onClick={() => setExportModalState(true)} color="blue">
-					Export
-				</Button>
-			</div>
+					return null;
+				})}
+			</Stack>
 			<footer id={styles.footer}>
 				<div className={styles.urls}>
 					<a href="https://github.com/aridevelopment-de/myanimetab">
