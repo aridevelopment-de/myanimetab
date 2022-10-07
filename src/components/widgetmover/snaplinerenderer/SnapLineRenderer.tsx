be88e19656d9dded2233c2aa878d3bd0e292931b
Ari24-cb24 <ari.publicmail@gmail.com>
2022-10-07 10:16:33 +0200

    Added SnapLineRenderer

---
+++ src/components/widgetmover/snaplinerenderer/SnapLineRenderer.tsx
@@ -0,0 +1,62 @@
+import { useLiveQuery } from "dexie-react-hooks";
+import { useSnapLineState } from "../../../hooks/widgetmover";
+import { metaDb } from "../../../utils/db";
+
+export const SnapLineRenderer = () => {
+    const snapLines = useLiveQuery(() => metaDb.snapLines.toArray(), []);
+    const [existsGlowSnapLine, glowLines] = useSnapLineState((state) => [state.existsGlowSnapLine, state.glownSnapLines]);
+    
+    if (!snapLines) return <></>;
+
+    return (
+        <>
+            {snapLines.map((snapLine) => {
+                let opacity = 0.3;
+
+                if (existsGlowSnapLine(snapLine.id, snapLine.axis)) {
+                    opacity = 1;
+                }
+
+                if (snapLine.axis === "horizontal") {
+                    return (
+                        <div
+                            key={snapLine.id}
+                            style={{
+                                position: "absolute",
+                                height: "5px",
+                                width: "100%",
+                                opacity: opacity,
+                                top: `${snapLine.top}%` || undefined,
+                                bottom: `${snapLine.bottom}%` || undefined,
+                                pointerEvents: "none",
+                                zIndex: 1000,
+                                backgroundColor: "red",
+                                boxShadow: opacity === 1 ? "0px 0px 10px red" : undefined,
+                            }}
+                        />
+                    );
+                } else if (snapLine.axis === "vertical") {
+                    return (
+                        <div
+                            key={snapLine.id}
+                            style={{
+                                position: "absolute",
+                                width: "5px",
+                                height: "100%",
+                                opacity: opacity,
+                                left: `${snapLine.left}%` || undefined,
+                                right: `${snapLine.right}%` || undefined,
+                                pointerEvents: "none",
+                                zIndex: 1000,
+                                backgroundColor: "red",
+                                boxShadow: opacity === 1 ? "0px 0px 10px red" : undefined,
+                            }}
+                        />
+                    );
+                }
+
+                return null;
+            })}
+        </>
+    );
+}
