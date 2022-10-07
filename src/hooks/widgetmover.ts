9aabdf67acba8ffef978446bf9a1ad72c59f3269
Ari24-cb24 <ari.publicmail@gmail.com>
2022-10-07 10:17:12 +0200

    Added widgetmover zustand hooks for selectedwidget and SnapLineStates (glowing)

--- src/hooks/widgetmover.ts
+++ src/hooks/widgetmover.ts
@@ -4,16 +4,52 @@ import { devtools } from 'zustand/middleware'
 export interface IWidgetMover {
     enabled: boolean;
     setEnabled: (enabled: boolean) => void;
-    selectedWidget: string;
-    setSelectedWidget: (widget: string) => void;
 }
 
+export interface IMoverSettings {
+    selectedWidget: string | null;
+    setSelectedWidget: (widget: string | null) => void;
+}
 
 export const useMoverState = create<IWidgetMover>()(
     devtools((set) => ({
-        enabled: false,
+        enabled: true, // TODO: defaults to false
         setEnabled: (enabled) => set({ enabled }),
-        selectedWidget: '',
+    })),
+)
+
+export const useMoverSettings = create<IMoverSettings>()(
+    devtools((set) => ({
+        selectedWidget: null,
         setSelectedWidget: (widget) => set({ selectedWidget: widget }),
     })),
 )
+
+export enum GlowPriority {
+    None = 0,
+    Snap = 1,
+    Hover = 2,
+    All = 3,
+}
+
+export interface ISnapLinesState {
+    glownSnapLines: {id: number, align: "horizontal" | "vertical", priority: GlowPriority}[];
+    existsGlowSnapLine: (id: number, align: "horizontal" | "vertical") => boolean;
+    addGlowSnapLine: (id: number, align: "horizontal" | "vertical", priority: GlowPriority) => void;
+    removeGlowSnapLine: (id: number, priority: GlowPriority) => void;
+    clearGlowSnapLines: (priority: GlowPriority) => void;
+    clearGlowVerticalSnapLines: (priority: GlowPriority) => void;
+    clearGlowHorizontalSnapLines: (priority: GlowPriority) => void;
+}
+
+export const useSnapLineState = create<ISnapLinesState>()(
+    devtools((set, get) => ({
+        glownSnapLines: [],
+        existsGlowSnapLine: (id, align) => get().glownSnapLines.some((line) => line.id === id && line.align === align),
+        addGlowSnapLine: (id, align: "horizontal" | "vertical", priority: GlowPriority) => set((state) => ({ glownSnapLines: [...state.glownSnapLines, {id, align, priority}] })),
+        removeGlowSnapLine: (id, priority: GlowPriority) => set((state) => ({ glownSnapLines: state.glownSnapLines.filter((i) => i.id !== id && i.priority <= priority) })),
+        clearGlowSnapLines: (priority: GlowPriority) => set((state) => ({ glownSnapLines: state.glownSnapLines.filter((i) => i.priority <= priority) })),
+        clearGlowVerticalSnapLines: (priority: GlowPriority) => set((state) => ({ glownSnapLines: state.glownSnapLines.filter((i) => i.align === "vertical" && i.priority <= priority) })),
+        clearGlowHorizontalSnapLines: (priority: GlowPriority) => set((state) => ({ glownSnapLines: state.glownSnapLines.filter((i) => i.align === "horizontal" && i.priority <= priority) })),
+    })),
+);
