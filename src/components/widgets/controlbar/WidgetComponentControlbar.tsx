447755b723486877daa4156e63fd65f881e28154
Ari24-cb24 <ari.publicmail@gmail.com>
2022-10-07 10:18:30 +0200

    Added WidgetMoverWrapper wrapper to clock and controlbar for testing purposes

--- src/components/widgets/controlbar/WidgetComponentControlbar.tsx
+++ src/components/widgets/controlbar/WidgetComponentControlbar.tsx
@@ -6,67 +6,67 @@ import LockOpenIcon from "@mui/icons-material/LockOpen";
 import SettingsIcon from "@mui/icons-material/Settings";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import { useEffect, useState } from "react";
-import { widgetsDb } from "../../../utils/db";
 import EventHandler from "../../../utils/eventhandler";
 import { useSetting } from "../../../utils/eventhooks";
 import { KnownComponent } from "../../../utils/registry/types";
+import WidgetMoverWrapper from "../../widgetmover/wrapper/WidgetMoverWrapper";
 import styles from "./controlbar.module.css";
-
-const positionValues = [styles.two, styles.one];
 
 function ControlBar(props: { blur: boolean; id: string }) {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [position, _] = useSetting(props.id, "position");
 	const [unlocked, setUnlocked] = useSetting("wallpaper-0", "state");
 	const [collapsed, setCollapsed] = useState(false);
 	useEffect(() => setCollapsed(props.blur), [props.blur]);
 
-	if (position === undefined) return <></>;
-
 	return (
-		<div
-			className={`${styles.control_menu} ${positionValues[position]}`}
-			style={{
-				transform: collapsed ? "translateY(-80%)" : "translateY(0)",
-			}}
-		>
-			<div className={styles.item__wrapper}>
-				<div
-					onClick={() =>
-						EventHandler.emit("settings_window_state", {
-							opened: true,
-						})
-					}
-				>
-					<SettingsIcon />
-				</div>
-			</div>
-
-			<div className={styles.item__wrapper}>
-				<div
-					onClick={() => {
-						EventHandler.emits(["skip_image", "playlist_refresh"]);
-						setUnlocked(true);
-					}}
-				>
-					<SkipNextIcon />
-				</div>
-			</div>
-
-			<div className={styles.item__wrapper}>
-				<div onClick={() => setUnlocked(!unlocked)}>
-					{unlocked ? <LockOpenIcon /> : <LockIcon />}
-				</div>
-			</div>
-
+		<WidgetMoverWrapper id={props.id}>
 			<div
-				className={`${styles.expand_less__wrapper} ${styles.item__wrapper}`}
+				className={`${styles.control_menu}`}
+				style={{
+					transform: collapsed ? "translateY(-80%)" : "translateY(0)",
+				}}
 			>
-				<div onClick={() => setCollapsed(!collapsed)}>
-					{collapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+				<div className={styles.item__wrapper}>
+					<div
+						onClick={() =>
+							EventHandler.emit("settings_window_state", {
+								opened: true,
+							})
+						}
+					>
+						<SettingsIcon />
+					</div>
+				</div>
+
+				<div className={styles.item__wrapper}>
+					<div
+						onClick={() => {
+							EventHandler.emits([
+								"skip_image",
+								"playlist_refresh",
+							]);
+							setUnlocked(true);
+						}}
+					>
+						<SkipNextIcon />
+					</div>
+				</div>
+
+				<div className={styles.item__wrapper}>
+					<div onClick={() => setUnlocked(!unlocked)}>
+						{unlocked ? <LockOpenIcon /> : <LockIcon />}
+					</div>
+				</div>
+
+				<div
+					className={`${styles.expand_less__wrapper} ${styles.item__wrapper}`}
+				>
+					<div onClick={() => setCollapsed(!collapsed)}>
+						{collapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+					</div>
 				</div>
 			</div>
-		</div>
+		</WidgetMoverWrapper>
 	);
 }
 
@@ -88,12 +88,6 @@ export default {
 		},
 	},
 	contentSettings: [
-		{
-			name: "Positioning",
-			key: "position",
-			type: "dropdown",
-			values: positionValues,
-			displayedValues: ["Right upper corner", "Left upper corner"],
-		},
+		
 	],
 } as KnownComponent;
