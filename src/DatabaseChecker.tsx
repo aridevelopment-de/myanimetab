03bca14b9c3982582e6ea09d203006fea4eaae05
Ari van Houten <ari.publicmail@gmail.com>
2023-01-11 16:53:04 +0100

    Fixed addMissing only being called when myanimetab is being initialized for the first time

--- src/DatabaseChecker.tsx
+++ src/DatabaseChecker.tsx
@@ -5,9 +5,9 @@ const DatabaseChecker = (props: { children: JSX.Element }) => {
 	const [shouldRender, setShouldRender] = useState<boolean>(false);
 
 	useEffect(() => {
+		addMissing();
+		
 		metaDb.initializeFirstTimers().then((newUser: boolean) => {
-			addMissing();
-
 			if (newUser) {
 				// Reload the page for new users as this ensures correct creation of databases
 				setTimeout(() => window.location.reload(), 500);
